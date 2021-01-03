38a6099284bdd3da1799d29351fe0cb559e0775e
eoinlynchcodes <eoin95lynch@gmail.com>
2021-01-03 20:57:35 +0000

    Daily-co is implemented but has bugs to be resolved.

--- frontend/README.md
+++ frontend/README.md
@@ -4,3 +4,17 @@ Routes:
 Prospects:
 
 {BACKEND}/
+
+
+Links on Daily.co implementation:
+https://github.com/daily-demos/call-object-react
+https://www.daily.co/blog/building-a-custom-video-chat-app-with-react/
+
+Files added from Daily.co project: 
+- Everything in dailyco-components folder.
+- logUtils.js
+- urlUtils.js
+- CallObjectContext.js
+- api.js
+
+
--- frontend/src/components/Videobox.js
+++ frontend/src/components/Videobox.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../dailyco-components/App/DailyApp';
+import DailyApp from '../dailyco-components/App/DailyApp';
+import DailyIframe from '@daily-co/daily-js';
 
 function Videobox(){
 
     return(
-        <div>
+        <div className="video-box">
             <h2>Videobox will go here.</h2>
+            <DailyApp/>
         </div>
     );
 }
