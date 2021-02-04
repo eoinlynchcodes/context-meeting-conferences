0e6d6b5bfa04430dfdff2dc5978877698b96244d
eoinlynchcodes <eoin95lynch@gmail.com>
2021-02-04 07:05:15 +0100

    Update Vercel.

--- frontend/src/components/Videobox.js
+++ frontend/src/components/Videobox.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from '../dailyco-components/App/DailyApp';
-// import DailyApp from '../dailyco-components/App/DailyApp';
 import DailycoApp from '../daily-co/DailycoApp';
 
 function Videobox(){
 
     return(
         <div className="video-box">
-            <h2>Videobox will go here.</h2>
             <DailycoApp/>
         </div>
     );
--- frontend/src/daily-co/DailycoApp.js
+++ frontend/src/daily-co/DailycoApp.js
@@ -5,11 +5,6 @@ function DailycoApp() {
   return (
     <div className="">
       <header className="">
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-     
-
         <VideoCallFrame
           url={ process.env.REACT_APP_DAILY_ROOM_URL }
         ></VideoCallFrame>
